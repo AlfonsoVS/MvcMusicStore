9cdc1d238ea6e074fb927d05ed67a0b0f8a39ab6
AlfonsoVS <avazquezsiu@gmail.com>
2017-12-08 16:22:53 +0100

    Update Global.Asax.cs

--- MvcMusicStore/Global.Asax.cs
+++ MvcMusicStore/Global.Asax.cs
@@ -1,1 +1,29 @@
-<%@ Application Codebehind="Global.asax.cs" Inherits="MvcMusicStore.MvcApplication" Language="C#" %>
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            /*
+            Database.SetInitializer(
+                new DropCreateDatabaseIfModelChanges<MusicStoreDB>());
+            */
+            Database.SetInitializer(new MusicStoreDbInitializer());
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
+
